a77d2b8d77966f8a4d058aedc4a325dc5430f17c
amir-313 <amirmulani0313@gmail.com>
2021-09-21 16:28:31 +0530

    simple joke

---
+++ script.js
@@ -0,0 +1,17 @@
+// assigninig vairables
+
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke)
+
+async function generateJoke(){
+    const config = {
+        headers : {
+            Accept : 'application/json'
+        }
+    }
+    const response = await fetch ('https://icanhazdadjoke.com', config);
+    const data = await response.json();
+    jokeEl.innerHTML = data.joke;
+}
